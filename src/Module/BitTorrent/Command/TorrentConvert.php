8889bea92877ec7100bcf57b497545cc99b0cb83
Marlin Cremers <marlinc@mms-projects.net>
2012-04-14 22:20:00 +0200

    Added a .torrent to Magnet URI converter

---
+++ src/Module/BitTorrent/Command/TorrentConvert.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Botlife\Module\BitTorrent\Command;
+
+use Botlife\Application\Colors;
+use Ircbot\Type\MessageCommand;
+
+class TorrentConvert extends \Botlife\Command\ACommand
+{
+
+    public $regex  = '/.*\.torrent$/i';
+    public $action = 'run';
+    public $code   = 'torrent-convert';
+    
+    public $responseType    = self::RESPONSE_PUBLIC;
+    
+    public function run(MessageCommand $event)
+    {
+        $this->detectResponseType($event->message, $event->target);
+        $C = new Colors;
+        $data = @file_get_contents($event->message);
+        if (!$data) {
+            return;
+        }
+        try {
+            $torrent = \PHP\BitTorrent\Torrent::createFromData($data);
+        } catch (InvalidArgumentException $e) {
+            return;
+        }
+        $this->respondWithInformation(array(
+        	'Magnet URI' => $C(Colors::STYLE_NORMAL, $torrent->getMagnet()))
+        );
+    }
+
+}
+    
