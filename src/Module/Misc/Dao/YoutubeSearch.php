b2e234fba3538e51d8025715f4a104db373ec7f4
Marlin Cremers <marlinc@mms-projects.net>
2012-04-17 21:54:35 +0200

    Added a rating and duration to the YouTube searchengine

--- src/Module/Misc/Dao/YoutubeSearch.php
+++ src/Module/Misc/Dao/YoutubeSearch.php
@@ -35,6 +35,15 @@ class YoutubeSearch extends \Botlife\Entity\SearchEngine
             $mediaGroup = $entry->{'media$group'};
             $result->id    = $mediaGroup->{'yt$videoid'}->{'$t'};
             $result->title = $entry->title->{'$t'};
+            $result->date  = new \DateTime(
+                $mediaGroup->{'yt$uploaded'}->{'$t'}
+            );
+            $result->votes  = (int) $entry->{'gd$rating'}->numRaters;
+            $result->rating = new \StdClass;
+            $result->rating->average = (int) $entry->{'gd$rating'}->average * 20;
+            $result->rating->likes = (int) $entry->{'yt$rating'}->numLikes;
+            $result->rating->dislikes = (int) $entry->{'yt$rating'}->numDislikes;
+            $result->duration = (int) $mediaGroup->{'yt$duration'}->{'seconds'}; 
             $result->description = $mediaGroup->{'media$description'}->{'$t'};
             $result->url   = 'http://youtu.be/' . $result->id;
             $info->entries[] = $result;
